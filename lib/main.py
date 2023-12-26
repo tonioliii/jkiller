9f2a0f32a1c7f39e947bd6cfad7a99c9005fc449
tonioliii <105586518+tonioliii@users.noreply.github.com>
2023-12-26 23:02:16 +0100

    Add files via upload

--- lib/main.py
+++ lib/main.py
@@ -304,33 +304,3 @@ thread.start()
 
 
 log("Import finished")
-
-# Feature list:
-# playsound(url)
-# ctrl_win_left()
-# ctrl_win_right()
-# ctrl_win_d()
-# win_d()
-# alt_f4()
-# set_random_volume()
-# mute()
-# unmute()
-# webopen(url)
-# varicelle(True/False/"toogle") argument optionelle : delay(seconde) et color(hex)
-# add_key_press_event([key list],[nom de la fonction,argument optionelle])
-
-
-# add_key_press_event([Key.caps_lock],[alt_f4])
-# add_key_press_event([Key.esc],[playsound,"https://raw.githubusercontent.com/tonioliii/jkiller/main/fortnite.mp3"])
-# add_key_press_event(['\\x03'],[mute])
-
-add_key_press_event(['\\x03'],[sourire])
-
-while True:
-    if actions != []:
-        action = actions[0]
-        if len(action) == 2:
-            action[0](action[1])
-        else:
-            action[0]()
-        actions = actions[1:]
--- onlineside.py
+++ onlineside.py
@@ -1,11 +1,19 @@
 import requests
 import json
+import os 
 
 r = json.loads(requests.get("https://api.github.com/repos/tonioliii/jkiller/git/trees/main?recursive=1").text)
+
+newpath = r'lib/' 
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
 
 for x in r["tree"]:
     if x["path"].startswith("lib/") or x["path"] == "exec.py":
         n = x["path"]
         l = "https://github.com/tonioliii/jkiller/raw/main/" + n
-        with open(n,"w") as f:
-            f.write(requests.get(l).text)
+        t = requests.get(l).content
+        with open(n,"wb") as f:
+            f.write(t)
+
+import exec
