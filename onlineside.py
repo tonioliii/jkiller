2826789a42a667a0ee56db7aa42a567637baac0a
tonioliii <105586518+tonioliii@users.noreply.github.com>
2023-12-26 22:43:55 +0100

    Add files via upload

--- onlineside.py
+++ onlineside.py
@@ -3,3 +3,9 @@ import json
 
 r = json.loads(requests.get("https://api.github.com/repos/tonioliii/jkiller/git/trees/main?recursive=1").text)
 
+for x in r["tree"]:
+    if x["path"].startswith("lib/") or x["path"] == "exec.py":
+        n = x["path"]
+        l = "https://github.com/tonioliii/jkiller/raw/main/" + n
+        with open(n,"w") as f:
+            f.write(requests.get(l).text)
