237111b7f2ed3b775ca4f685d1bfbd57a89899d1
tonioliii <105586518+tonioliii@users.noreply.github.com>
2023-12-27 13:03:52 +0100

    Update exec.py

--- exec.py
+++ exec.py
@@ -1,23 +1,6 @@
 from lib.main import *
 from lib.geometry import *
 
-# Feature list:
-# playsound(url)
-# ctrl_win_left()
-# ctrl_win_right()
-# ctrl_win_d()
-# win_d()
-# alt_f4()
-# set_random_volume()
-# mute()
-# unmute()
-# webopen(url)
-# varicelle(True/False/"toogle") argument optionelle : delay(seconde) et color(hex)
-# set_sourire_on_screen() argument optionelle : color(hex)
-# set_background(image_url)
-
-# add_key_press_event([key list],[nom de la fonction,argument optionelle])
-
 
 def on_press(key):
     try:
