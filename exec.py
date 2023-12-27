4a19d443dfdc9c15d6dd40b6b8c333e4e2bd17fc
tonioliii <105586518+tonioliii@users.noreply.github.com>
2023-12-27 12:23:17 +0100

    Add files via upload

--- exec.py
+++ exec.py
@@ -13,14 +13,31 @@ from lib.geometry import *
 # unmute()
 # webopen(url)
 # varicelle(True/False/"toogle") argument optionelle : delay(seconde) et color(hex)
+# set_sourire_on_screen() argument optionelle : color(hex)
+
 # add_key_press_event([key list],[nom de la fonction,argument optionelle])
 
+
+def on_press(key):
+    try:
+        if type(key) != type(Key.alt):
+            key = str(key).replace("'","")
+        last_keys.insert(0,key)
+        for x in keypress_event :
+            if startswithlist(last_keys,x[0]):        
+                actions.append(x[1])
+    except Exception as E:
+        log(f"Error {E} on_press({key})")
+
+listener = Listener(on_press=on_press)
+listener.start()
 
 # add_key_press_event([Key.caps_lock],[alt_f4])
 # add_key_press_event([Key.esc],[playsound,"https://raw.githubusercontent.com/tonioliii/jkiller/main/fortnite.mp3"])
 # add_key_press_event(['\\x03'],[mute])
+add_key_press_event(['\\x03'],[set_sourire_on_screen])
+add_key_press_event([Key.caps_lock],[set_sourire_on_screen,0xffe838])
 
-add_key_press_event(['\\x03'],[sourire])
 
 while True:
     if actions != []:
