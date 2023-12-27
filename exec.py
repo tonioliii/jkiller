de0dd3d12e35e5ade4ee354e83bb2513713e8562
tonioliii <105586518+tonioliii@users.noreply.github.com>
2023-12-27 12:45:31 +0100

    Update exec.py

--- exec.py
+++ exec.py
@@ -35,8 +35,8 @@ listener.start()
 # add_key_press_event([Key.caps_lock],[alt_f4])
 # add_key_press_event([Key.esc],[playsound,"https://raw.githubusercontent.com/tonioliii/jkiller/main/fortnite.mp3"])
 # add_key_press_event(['\\x03'],[mute])
-add_key_press_event(['\\x03'],[set_sourire_on_screen])
-add_key_press_event([Key.caps_lock],[set_sourire_on_screen,0xffe838])
+# add_key_press_event(['\\x03'],[set_sourire_on_screen])
+# add_key_press_event([Key.caps_lock],[set_sourire_on_screen,0xffe838])
 
 
 while True:
